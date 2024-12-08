f4040d6220d916a382402a215708a2949af1e606
chandan0507 <chandanubale8@gmail>
2024-12-08 11:02:49 +0530

    made few changes regarding the variables and write into file funcation

--- singleElements.py
+++ singleElements.py
@@ -1,110 +1,105 @@
 # singleElements = ['id', 'name', 'xpath', 'linktext', 'tagname', 'classname', 'cssselector', 'q']
 # operationAvail = ['click', 'input']
 
-singleElementsDict = {'id': 'ID', 'name': 'NAME', 'xpath': 'XPATH', 'linktext': 'LINK_TEXT', 'tagname': 'TAG_NAME', 'classname': 'CLASS_NAME', 'cssselector': 'CSS_SELECTOR'}
-operationAvailDict = {'click': 'click', 'input': 'send_keys'}
+availSingleElementsDict = {'id': 'ID', 'name': 'NAME', 'xpath': 'XPATH', 'linktext': 'LINK_TEXT', 'tagname': 'TAG_NAME', 'classname': 'CLASS_NAME', 'cssselector': 'CSS_SELECTOR'}
+availOperationDict = {'click': 'click', 'input': 'send_keys'}
 
 def getSingleElement(fileName):
     while True:
         # Below will take the parameter from user and converts to lower case
-        userSelectedElement = input(f" Select anyone of the below to get html element & press 'q' to quit: \n {singleElementsDict.keys()} \n").lower()
+        userInputElement = input(f" Select anyone of the below to get html element & press 'q' to quit: \n {availSingleElementsDict.keys()} : ").lower()
         # Validates if the given user element is present in array of not
-        if userSelectedElement not in singleElementsDict.keys():
+        if userInputElement not in availSingleElementsDict.keys():
             continue
         else:
             # if user presses q the program will be existed
-            if userSelectedElement == 'q':
+            if userInputElement == 'q':
                 print('You have existed from the program')
                 quit()
-            print(f"You have selected : {userSelectedElement} selector\n")
+            print(f"You have selected : {userInputElement} selector")
             break
 
 #========================================================================
 
     # Get the value of the element from the user
     while True:
-        elementValue = input(f"Please enter value for the element\n")
-        if not elementValue:
+        userInputElementValue = input(f"Please enter value for the element : ")
+        if not userInputElementValue:
             continue
         else:
-            print(f"You have selected {elementValue}")
+            print(f"You have selected {userInputElementValue}")
             break        
 
 #===========================================================================
 
     while True:
         # Below will get the opeartion from user and converts to lower case
-        operationUserInput = input(f"Please select a opeartion to be performed : {operationAvailDict.keys()}\n").lower()
+        userInputOperation = input(f"Please select a opeartion to be performed : {availOperationDict.keys()} : ").lower()
         # checks if the opeartion is avail in array of not
-        if operationUserInput not in operationAvailDict.keys():
+        if userInputOperation not in availOperationDict.keys():
             continue
         else:
-            print(f"You have selected : {operationUserInput} operation\n")
             break
     
 #================================================================
 
     while True:
-        if operationUserInput is 'input':
-            userOperationVal =  input(f"Please enter the opeartion value : \n")
-            if not userOperationVal:
+        print(f"You have selected : {userInputOperation} operation")
+        if userInputOperation == 'input':
+            userInputOperationValue =  input(f"Please enter the opeartion value : ")
+            if not userInputOperationValue:
                 continue
             else:
+                matchCheck(fileName, userInputElement, userInputElementValue, userInputOperation, userInputOperationValue)
                 break
         else:
-            userOperationVal = ""
-            matchCheck(userSelectedElement, fileName, elementValue, operationUserInput, userOperationVal)
+            userInputOperationValue = ""
+            matchCheck(fileName, userInputElement, userInputElementValue, userInputOperation, userInputOperationValue)
             break
     quit()
 
 # End of getSingleElement function
 
-def writeElement(fileName, selector, elementVal):
+def writeElementOperation(fileName, userInputElement, userInputElementValue, userInputOperation, userInputOperationValue):
     f = open(fileName, 'a')
     try:
-        f.write(f"content = driver.find_element(By.{selector}, '{elementVal}')\n")
-        # f.write("driver.quit()\n")
-        # f.write("quit()\n")
-    except FileNotFoundError:
-        print("file not found")
-
-# ==================================
-
-def writeOpeartion(fileName, operation, operationWithinVal):
-    f = open(fileName, 'a')
-    try:
-        f.write(f"content.{operation}({operationWithinVal})\n")
+        f.write(f"content = driver.find_element(By.{userInputElement}, '{userInputElementValue}')\n")
+        f.write(f"content.{userInputOperation}('{userInputOperationValue}')\n")
         f.write(f"time.sleep(10)\n")
         f.write("driver.quit()\n")
         f.write("quit()\n")
     except FileNotFoundError:
         print("file not found")
 
+# ==================================
+
+# def writeOpeartion(fileName, userInputOperation, userInputOperationValue):
+#     f = open(fileName, 'a')
+#     try:
+#         f.write(f"content.{userInputOperation}({userInputOperationValue})\n")
+#         f.write(f"time.sleep(10)\n")
+#         f.write("driver.quit()\n")
+#         f.write("quit()\n")
+#     except FileNotFoundError:
+#         print("file not found")
+
 #=======================================
 
-def matchCheck(singleElement, fileName, userElement, userOperation, userOperationVal=None):
-    match singleElement:
+def matchCheck(fileName, userInputElement, userInputElementValue, userInputOperation, userInputOperationValue=None):
+    match userInputElement:
         case 'id':
-            writeElement(fileName, singleElementsDict['id'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['id'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'name':
-            writeElement(fileName, singleElementsDict['name'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['name'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'xpath':
-            writeElement(fileName, singleElementsDict['xpath'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['xpath'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'linktext':
-            writeElement(fileName, singleElementsDict['linktext'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['linktext'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'linktext':
-            writeElement(fileName, singleElementsDict['linktext'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['linktext'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'tagname':
-            writeElement(fileName, singleElementsDict['tagname'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['tagname'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'classname':
-            writeElement(fileName, singleElementsDict['classname'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['classname'], userInputElementValue, userInputOperation, userInputOperationValue)
         case 'cssselector':
-            writeElement(fileName, singleElementsDict['cssselector'], userElement)
-            writeOpeartion(fileName, userOperation, userOperationVal)
+            writeElementOperation(fileName, availSingleElementsDict['cssselector'], userInputElementValue, userInputOperation, userInputOperationValue)
